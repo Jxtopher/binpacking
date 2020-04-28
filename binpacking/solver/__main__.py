6e7b4bcf7f70ad883e0700689b1286ed6121cd9b
Jxtopher <39927513+Jxtopher@users.noreply.github.com>
2020-04-28 19:14:07 +0200

    update neighbordhood

--- binpacking/solver/__main__.py
+++ binpacking/solver/__main__.py
@@ -7,12 +7,12 @@ from binpacking.plot.plot_handler import PlotHandler
 
 if __name__ == '__main__':
     bin_packing = InstanceLoader.get_bin_packing('binpacking2d-6-1.json')
-    neighborhood = Neighborhood(bin_packing)
+    # neighborhood = Neighborhood(bin_packing)
     sol_init = Solution(bin_packing.get_instance_size())
 
     tabu_size = 5
     max_iterations = 300
-    ts = TabuSearch(bin_packing, tabu_size, max_iterations, neighborhood)
+    ts = TabuSearch(bin_packing, tabu_size, max_iterations, Neighborhood.find_one_mutation_neighbor)
     s_star = ts.run(sol_init)
     print('best solution')
     print(s_star)
