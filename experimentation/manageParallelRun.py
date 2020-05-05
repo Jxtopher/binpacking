841890902fa6695a1244493e9acdc7201534afb2
Jxtopher <39927513+Jxtopher@users.noreply.github.com>
2020-05-05 12:59:26 +0200

    class

--- experimentation/manageParallelRun.py
+++ experimentation/manageParallelRun.py
@@ -10,57 +10,61 @@ from logging import getLogger
 
 log = getLogger(__name__)
 
-# run_function	the function to be performed
-# cmds_exec	list of function arguments
-# number_of_processes Number of processes to use
-# stdout	"" ->  stdout | "data.log" output file
-# ret		False -> not retrieve the outputs of the run_worker function
-def run_parallel(
-    run_function: Callable[..., str],
-    cmds_exec: List[Any],
-    number_of_processes: Optional[int] = None,
-    stdout: str = '',
-    ret: bool = False,
-) -> List[Any]:
-    if stdout != '':
-        sys.stdout = open(stdout, 'a')
 
-    if number_of_processes is None:
-        number_of_processes = os.cpu_count()
+class manageParallelRun:
 
-    startTime = time.time()
-    ret_info = []
-    pool = Pool(processes=number_of_processes)
+    # run_function	the function to be performed
+    # cmds_exec	list of function arguments
+    # number_of_processes Number of processes to use
+    # stdout	"" ->  stdout | "data.log" output file
+    # ret		False -> not retrieve the outputs of the run_worker function
+    @staticmethod
+    def run_parallel(
+        run_function: Callable[..., str],
+        cmds_exec: List[Any],
+        number_of_processes: Optional[int] = None,
+        stdout: str = '',
+        ret: bool = False,
+    ) -> List[Any]:
+        if stdout != '':
+            sys.stdout = open(stdout, 'a')
 
-    shuffle(cmds_exec)
+        if number_of_processes is None:
+            number_of_processes = os.cpu_count()
 
-    log.debug('[+] Nomber of execution ' + str(len(cmds_exec)))
-    log.debug('[+] ' + str(cmds_exec[0]))
+        startTime = time.time()
+        ret_info = []
+        pool = Pool(processes=number_of_processes)
 
-    try:
-        if ret:
-            jobs = pool.map_async(run_function, cmds_exec)
-            pool.close()
-            ret_info = jobs.get()
-        else:
-            pool.map_async(run_function, cmds_exec)
-            pool.close()
-    except KeyboardInterrupt:
-        log.info('parent received control-c')
-        pool.terminate()
+        shuffle(cmds_exec)
 
-    log.debug('[+] Execution time ' + str(timedelta(seconds=(time.time() - startTime))))
-    if stdout != '':
-        if ret_info != []:
-            print(ret_info)
-        sys.stdout = sys.__stdout__
-    return ret_info
+        log.debug('[+] Nomber of execution ' + str(len(cmds_exec)))
+        log.debug('[+] ' + str(cmds_exec[0]))
 
+        try:
+            if ret:
+                jobs = pool.map_async(run_function, cmds_exec)
+                pool.close()
+                ret_info = jobs.get()
+            else:
+                pool.map_async(run_function, cmds_exec)
+                pool.close()
+        except KeyboardInterrupt:
+            log.info('parent received control-c')
+            pool.terminate()
 
-# function run_worker execute des prgrammes exterieurs avec des
-# parametres command_line
-def run_worker(command_line: str) -> Tuple[int, str]:
-    try:
-        return getstatusoutput(command_line)
-    except KeyboardInterrupt:
-        return (0, 'Cancelled execution')
+        log.debug('[+] Execution time ' + str(timedelta(seconds=(time.time() - startTime))))
+        if stdout != '':
+            if ret_info != []:
+                print(ret_info)
+            sys.stdout = sys.__stdout__
+        return ret_info
+
+    # function run_worker execute des prgrammes exterieurs avec des
+    # parametres command_line
+    @staticmethod
+    def run_worker(command_line: str) -> Tuple[int, str]:
+        try:
+            return getstatusoutput(command_line)
+        except KeyboardInterrupt:
+            return (0, 'Cancelled execution')
