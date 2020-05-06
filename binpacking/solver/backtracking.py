99ffb28274506b1dc8488ea7b2cddc90c0b37317
Jxtopher <39927513+Jxtopher@users.noreply.github.com>
2020-05-06 22:12:55 +0200

    Draft of backtracking

---
+++ binpacking/solver/backtracking.py
@@ -0,0 +1,40 @@
+import copy
+from random import randint
+from binpacking.solver.bin_packing_2d import BinPacking2D
+from binpacking.solver.solution import Solution, Coordinate
+from binpacking.solver.optimisation_algo import OptimisationAlgo
+from binpacking.plot.plot_handler import PlotHandler
+
+
+class Backtracking(OptimisationAlgo):
+    def __init__(self, bin_packing: BinPacking2D):
+        self.bin_packing = bin_packing
+
+    def run(self, sol: Solution) -> Solution:
+        self.backtracking(sol)
+        return sol
+
+    def backtracking(self, sol: Solution, number_of_valid_items: int = 0) -> None:
+        print("test")
+        print(sol[0])
+        # self.Backtracking(sol, number_of_valid_items)
+
+        for x in range(0, self.bin_packing.capacity.get_width_height()[0]):
+            for y in range(0, self.bin_packing.capacity.get_width_height()[1]):
+                for r in [0, 90]:
+                    sol[number_of_valid_items] = Coordinate(x, y)
+                    if r == 90:
+                        sol[number_of_valid_items].rotate()
+                    self.bin_packing.evaluate(sol)
+                    if number_of_valid_items < len(sol) - 1:
+                        if 0 <= sol.get_fitness():
+                            self.backtracking(copy.deepcopy(sol), number_of_valid_items + 1)
+                    else:
+                        if sol.get_fitness() == len(sol):
+                            print(sol)
+                            plot_handler = PlotHandler(self.bin_packing, sol)
+                            plot_handler.save_to_file(
+                                'test_backtracking' + str(randint(0, 1000000000)) + '.png'
+                            )
+
+        pass
---
+++ tests/binpacking/solver/test_backtracking.py
@@ -0,0 +1,32 @@
+from tests.base import BaseTestCase
+
+from binpacking.solver.bin_packing_2d import BinPacking2D, Rectangle
+from binpacking.solver.solution import Solution, Coordinate
+from binpacking.solver.backtracking import Backtracking
+
+
+class BacktrackingTest(BaseTestCase):
+    def test_run(self) -> None:
+        instance = BinPacking2D(
+            Rectangle(100, 100),
+            [
+                Rectangle(30, 40),
+                Rectangle(70, 30),
+                Rectangle(30, 30),
+                Rectangle(40, 70),
+                Rectangle(30, 20),
+                Rectangle(30, 70),
+            ],
+        )
+        sol = Solution(instance.get_instance_size())
+        sol[0] = Coordinate(-1, -1)
+        sol[1] = Coordinate(-1, -1)
+        sol[2] = Coordinate(-1, -1)
+        sol[3] = Coordinate(-1, -1)
+        sol[4] = Coordinate(-1, -1)
+        sol[5] = Coordinate(-1, -1)
+
+        backtracking = Backtracking(instance)
+        backtracking.run(sol)
+
+        pass
