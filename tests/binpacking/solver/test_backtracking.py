c337b4263a86c6cf2bfdb036bfa9f2a1d9bbf6d9
Jxtopher <39927513+Jxtopher@users.noreply.github.com>
2020-05-09 20:52:24 +0200

    Update test_backtracking.py

--- tests/binpacking/solver/test_backtracking.py
+++ tests/binpacking/solver/test_backtracking.py
@@ -2,7 +2,8 @@ from tests.base import BaseTestCase
 
 from binpacking.solver.bin_packing_2d import BinPacking2D, Rectangle
 from binpacking.solver.solution import Solution, Coordinate
-from binpacking.solver.backtracking import Backtracking
+
+# from binpacking.solver.backtracking import Backtracking
 
 
 class BacktrackingTest(BaseTestCase):
