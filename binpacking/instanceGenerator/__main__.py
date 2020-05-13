c12e0ec380e1a02a4130399b76841d7bba4980b6
Jxtopher <39927513+Jxtopher@users.noreply.github.com>
2020-05-13 21:36:46 +0200

    Update __main__.py

--- binpacking/instanceGenerator/__main__.py
+++ binpacking/instanceGenerator/__main__.py
@@ -1,2 +1,10 @@
+from typing import Any, Dict
+from random import randint
+
 if __name__ == '__main__':
-    pass
+    instance_size = randint(0, 100)
+    instance: Dict[str, Any] = {}
+    instance["instance_number"] = instance_size
+    instance["capcity"] = [randint(0, 100), randint(0, 100)]
+    instance["items"] = [[randint(0, 10) for i in range(3)] for i in range(instance_size)]
+    print(instance)
