86e51791c7f41c5c4b6d9f23d1419086dfb497bd
david-sk <david.desmeurs@slagkryssaren.com>
2020-05-23 23:26:47 +0200

    AC3!

---
+++ tests/binpacking/solver/arcconsistency/test_ac3.py
@@ -0,0 +1,34 @@
+from tests.base import BaseTestCase
+
+from binpacking.solver.bin_packing_2d import BinPacking2D, Rectangle
+from binpacking.solver.solution import Solution, Coordinate
+from binpacking.solver.arcconsistency.ac3 import AC3
+
+
+class AC3Test(BaseTestCase):
+    def test_ac3_with_all_fitting_inputs(self) -> None:
+        instance = BinPacking2D(
+            Rectangle(100, 100), [Rectangle(10, 10), Rectangle(20, 20), Rectangle(10, 30)],
+        )
+        sol = Solution(instance.get_instance_size())
+
+        ac3 = AC3(instance)
+        domains = ac3.run(sol)
+        self.assertEqual(sum(len(sub_domain) for sub_domain in domains.values()), 42609)
+
+    def test_ac3_without_all_fitting_inputs(self) -> None:
+        instance = BinPacking2D(
+            Rectangle(10, 10), [Rectangle(1, 1), Rectangle(8, 8), Rectangle(3, 3)],
+        )
+        sol = Solution(instance.get_instance_size())
+
+        ac3 = AC3(instance)
+        domains = ac3.run(sol)
+        self.assertEqual(
+            domains,
+            {
+                0: set([Coordinate(-1, -1)]),
+                1: set([Coordinate(-1, -1)]),
+                2: set([Coordinate(-1, -1)]),
+            },
+        )
