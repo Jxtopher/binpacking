f87dd9238daced3537ead67ced9f94f2f39a264a
Jxtopher <39927513+Jxtopher@users.noreply.github.com>
2020-06-10 16:47:42 +0200

    Update optimisation.py

--- binpacking/solver/optimisation/optimisation.py
+++ binpacking/solver/optimisation/optimisation.py
@@ -2,6 +2,7 @@ from abc import ABCMeta, abstractmethod
 from typing import List
 
 from binpacking.solver.solution import Solution
+from binpacking.solver.domains import Domains
 
 
 class Optimisation:
@@ -9,5 +10,5 @@ class Optimisation:
     __metaclass__ = ABCMeta
 
     @abstractmethod
-    def run(self, sol: Solution) -> List[Solution]:
+    def run(self, sol: Solution, domains: Domains = None) -> List[Solution]:
         pass
